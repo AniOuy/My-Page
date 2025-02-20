abe1ec146007ae5927f5545dfb68fe0c139ff327
AniOuy <2papperlapapp@gmail.com>
2025-02-20 18:20:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('github').addEventListener('click', function(){
+    window.location.href = "https://github.com/AniOuy/";
+});
+
+document.getElementById('snap').addEventListener('click', function(){
+    window.location.href = "https://www.snapchat.com/add/AniOuy";
+});
